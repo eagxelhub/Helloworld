e27988ab64a1970ae74bf3528f0fafc0e2acfdfd
Alejandro Aguilar <alejandro.aguilar@zapopan.tecmm.edu.mx>
2019-01-17 19:34:30 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by Alex on 1/17/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	
+	
+	
+	std::cout << "Hello, World!\n";
+	return 0;
+}
